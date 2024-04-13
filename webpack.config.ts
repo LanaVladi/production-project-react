c90fd72ba47f4ef225bb72a28f645beb3bf12224
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-13 12:08:24 +0500

    fix: config decomposition, configuration options

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,21 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const webpack = require('webpack');
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths } from "./config/build/types/config";
 
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
+}
 
-const config: webpack.Configuration = {
-    mode: 'development',     // mode: 'production',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),  // entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js',  // для динамичного изменения имени filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    new webpack.ProgressPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+const mode = "development";
+const isDev = mode === "development";
 
-};
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: "development",
+    paths: paths,
+    isDev
+})
+
 export default config;
