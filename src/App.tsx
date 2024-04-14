49546322ea74700e59f1e754c38ae8344c755d1f
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-14 13:48:30 +0500

    add: classNames helper function

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import './/styles/index.scss';
 import { Link } from "react-router-dom";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import { Suspense, useContext, useState } from "react";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 
 
@@ -14,7 +14,7 @@ function App() {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
@@ -25,7 +25,7 @@ function App() {
                 </Routes>
             </Suspense>
             <Counter />
-        </div>
+        </ div>
     )
 }
 
