7c1f142319fa2433dcc176ff1cd498d9d8e4267c
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-24 22:23:05 +0500

    fix: remove empty file

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,5 @@
-import { classNames } from '../../../../shared/lib/classNames/classNames';
 import { Modal } from '../../../../shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import clss from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string;
@@ -11,7 +9,7 @@ interface LoginModalProps {
 
 export function LoginModal({ className, isOpen, onClose }: LoginModalProps) {
     return (
-        <Modal className={classNames(clss.loginModal, {}, [className])} isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose}>
             <LoginForm />
         </Modal>
     );
