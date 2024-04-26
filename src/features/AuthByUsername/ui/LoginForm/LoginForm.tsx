c41b4c7f035dc9b9ed64fc7dec6c4f7fb0927521
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-26 13:18:30 +0500

    add: DynamicModuleLoader

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next';
 import FocusLock from 'react-focus-lock';
 import { memo, useCallback, useEffect } from 'react';
 import { useDispatch, useSelector, useStore } from 'react-redux';
+import { AnyAction } from '@reduxjs/toolkit';
+import { DynamicModuleLoader } from '../../../../shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { ReduxStoreWithManager } from '../../../../app/providers/StoreProvider';
 import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
 import { Input } from '../../../../shared/ui/Input/Input';
@@ -22,27 +24,10 @@ export interface LoginFormProps {
 const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
-    const store = useStore() as ReduxStoreWithManager; // получаем наш стор
-
     const username = useSelector(getLoginUsername);
     const password = useSelector(getLoginPassword);
     const isLoading = useSelector(getLoginIsLoading);
     const error = useSelector(getLoginError);
-
-    useEffect(() => {
-        store.reducerManager.add('LoginForm', loginReducer);
-        dispatch({ type: '@INIT Login Form' });
-
-        return () => {
-            store.reducerManager.remove('LoginForm');
-            dispatch({ type: '@DESTROY Login Form' });
-        };
-        // eslint-disable-next-line
-    }, []); // =>  DynamicModuleLoader
-    // В момент, когда мы монтируем компонент мы добавляем редюсер, когда компонент размонтируется реактом и
-    //  он уже не нужен мы редюсер снова удаляем. // Eslint ругается на пустой массив зависимостей, но нам они там не нужны,
-    // т.к. этот useEffect должен отрабатывать один раз при монтировании компонента и можем эту строчку отключить
-    // eslint-disable-next-line
 
     const handleChangeUsername = useCallback((value: string) => {
         dispatch(loginActions.setUsername(value));
@@ -57,36 +42,39 @@ const LoginForm = memo(({ className }: LoginFormProps) => {
     }, [dispatch, password, username]);
 
     return (
-        <div className={classNames(clss.loginForm, {}, [className])}>
-            <Text title={t('Authorization form')} theme={TextTheme.PRIMARY} />
-            {error && <Text text={t('Incorrect username or password')} theme={TextTheme.ERROR} />}
-            <FocusLock>
-                <Input
-                    type="text"
-                    className={classNames(clss.input)}
-                    placeholder={t('Enter username')}
-                    onChange={handleChangeUsername}
-                    value={username}
-                />
-                <Input
-                    type="text"
-                    className={classNames(clss.input)}
-                    placeholder={t('Enter password')}
-                    onChange={handleChangePassword}
-                    value={password}
-                />
-            </FocusLock>
+        // eslint-disable-next-line i18next/no-literal-string
+        <DynamicModuleLoader removeAfterUnmount name="LoginForm" reducer={loginReducer}>
+            <div className={classNames(clss.loginForm, {}, [className])}>
+                <Text title={t('Authorization form')} theme={TextTheme.PRIMARY} />
+                {error && <Text text={t('Incorrect username or password')} theme={TextTheme.ERROR} />}
+                <FocusLock>
+                    <Input
+                        type="text"
+                        className={classNames(clss.input)}
+                        placeholder={t('Enter username')}
+                        onChange={handleChangeUsername}
+                        value={username}
+                    />
+                    <Input
+                        type="text"
+                        className={classNames(clss.input)}
+                        placeholder={t('Enter password')}
+                        onChange={handleChangePassword}
+                        value={password}
+                    />
+                </FocusLock>
 
-            <Button
-                className={classNames(clss.loginbtn)}
-                theme={ButtonTheme.OUTLINE}
-                type="button"
-                onClick={handleClickSignIn}
-                disabled={isLoading}
-            >
-                {t('Sign in')}
-            </Button>
-        </div>
+                <Button
+                    className={classNames(clss.loginbtn)}
+                    theme={ButtonTheme.OUTLINE}
+                    type="button"
+                    onClick={handleClickSignIn}
+                    disabled={isLoading}
+                >
+                    {t('Sign in')}
+                </Button>
+            </div>
+        </DynamicModuleLoader>
     );
 });
 
---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,46 @@
+import { Reducer } from '@reduxjs/toolkit';
+import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+import { FC, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+
+interface DynamicModuleLoaderProps {
+    name: StateSchemaKey;
+    reducer: Reducer;
+    removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+    const {
+        children,
+        name, reducer,
+        removeAfterUnmount,
+    } = props;
+
+    const dispatch = useDispatch();
+    const store = useStore() as ReduxStoreWithManager; // получаем наш стор
+
+    useEffect(() => {
+        store.reducerManager.add(name, reducer);
+        dispatch({ type: `@INIT ${name} reducer` });
+
+        return () => {
+            if (removeAfterUnmount) {
+                store.reducerManager.remove(name);
+                dispatch({ type: `@DESTROY ${name} reducer` });
+            }
+        };
+    // eslint-disable-next-line
+}, []); // =>  DynamicModuleLoader
+    // В момент, когда мы монтируем компонент мы добавляем редюсер, когда компонент размонтируется реактом и
+    //  он уже не нужен мы редюсер снова удаляем. // Eslint ругается на пустой массив зависимостей, но нам они там не нужны,
+    // т.к. этот useEffect должен отрабатывать один раз при монтировании компонента и можем эту строчку отключить
+    // eslint-disable-next-line
+
+    return (
+        // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            {children}
+        </>
+    );
+};
