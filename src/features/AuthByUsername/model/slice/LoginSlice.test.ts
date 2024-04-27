a0a4774066e276f712807408eefe592dad9d13c7
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-27 16:25:58 +0500

    add: LoginSlice.test.ts

---
+++ src/features/AuthByUsername/model/slice/LoginSlice.test.ts
@@ -0,0 +1,15 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginSchema';
+import { loginActions, loginReducer } from './LoginSlice';
+
+describe('LoginSlice.test', () => {
+    test('should return decrement value', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('555'))).toEqual({ username: '555' });
+    });
+
+    test('should return increment value', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('333'))).toEqual({ password: '333' });
+    });
+});
