7c6ce88c2af4f4a9796a1b2348299de53dc7c2a9
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-29 14:15:24 +0500

    fix: unit test

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -13,7 +13,6 @@ describe('counterSlice.test', () => {
     });
 
     test('should work with empty state', () => {
-        const state: CounterSchema = undefined;
-        expect(counterReducer(state, counterActions.increment())).toEqual({ value: 1 }); // initialState + 1
+        expect(counterReducer(undefined, counterActions.increment())).toEqual({ value: 1 }); // initialState + 1
     });
 });
