2678f2e80e8bb33a24194aac6271d17a34d39f10
Svetlana Vladi <lanavladi05@gmail.com>
2024-05-03 19:39:55 +0500

    add: comment entity

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import { Text } from '../../../../shared/ui/Text/Text';
+import { CommentList } from '../../../../entities/Comment';
 import { ArticleDetails } from '../../../../entities/Article';
 import { classNames } from '../../../../shared/lib/classNames/classNames';
 import clss from './ArticleDetailsPage.module.scss';
@@ -11,7 +13,7 @@ interface ArticleDetailsPageProps {
 
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
-    const { t } = useTranslation('article');
+    const { t } = useTranslation();
     const { id } = useParams<{id: string}>();
 
     if (!id) {
@@ -25,6 +27,30 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     return (
         <div className={classNames(clss.articleDetailsPage, {}, [className])}>
             <ArticleDetails id={id} />
+            <Text className={clss.commentTitle} title={t('Comments')} />
+            <CommentList
+                isLoading
+                comments={[
+                    {
+                        id: '1',
+                        text: 'comment1',
+                        user: {
+                            id: '1',
+                            username: 'Ulbi',
+                            avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
+                        },
+                    },
+                    {
+                        id: '4',
+                        text: 'comment2',
+                        user: {
+                            id: '4',
+                            username: 'Yan',
+                            avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
+                        },
+                    },
+                ]}
+            />
         </div>
     );
 };
