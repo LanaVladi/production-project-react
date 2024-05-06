5402b953ea91e98abb5f574280c5ab389dcc8aad
Svetlana Vladi <lanavladi05@gmail.com>
2024-05-06 11:57:57 +0500

    add: model forarticles and view switching

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -10,6 +10,7 @@ import { LoginSchema } from '../../../../features/AuthByUsername/ui';
 import { CounterSchema } from '../../../../entities/Counter';
 import { UserSchema } from '../../../../entities/User';
 import { ArticleDetailsCommentsSchema } from '../../../../pages/ArticleDetailsPage/model/types/ArticleDetailsCommentsSchema';
+import { ArticlesPageSchema } from '../../../../pages/ArticlesPage';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -21,6 +22,7 @@ export interface StateSchema {
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
     addCommentForm?: AddCommentFormSchema;
+    articlesPage?: ArticlesPageSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
