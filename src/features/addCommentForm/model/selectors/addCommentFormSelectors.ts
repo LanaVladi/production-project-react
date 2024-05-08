b2fb59b241089de7895d11a318d7758c272125a7
Svetlana Vladi <lanavladi05@gmail.com>
2024-05-08 23:26:58 +0500

    fix: AddCommentForm

--- src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,4 +1,5 @@
 import { StateSchema } from '../../../../app/providers/StoreProvider';
 
-export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text;
+export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text ?? '';
+// если использвать ??, то при вернет правую часть, если левая будет null или undefined
 export const getAddCommentFormError = (state: StateSchema) => state.addCommentForm?.error;
