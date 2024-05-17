4571b9f5ba1456190c1fee331031d281e4caae6c
Svetlana Vladi <lanavladi05@gmail.com>
2024-05-18 00:04:24 +0500

    add: Dropdown from headlessui, update navbar, add menu with avatar

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -38,7 +38,7 @@ export const CountrySelect = memo(({
             value={value}
             onChange={onChangeHandler}
             readonly={readonly}
-            direction="bottom"
+            direction="bottom right"
         />
     );
 });
