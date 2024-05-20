0b5a8e36fc4749eb0b067ad9b372b6a2a3ad14c2
Svetlana Vladi <lanavladi05@gmail.com>
2024-05-20 17:12:53 +0500

    refactor: remove Popover to NotificationButton, Dropdown to AvatarDropdown

---
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,58 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dropdown } from '../../../../shared/ui/Popups';
+import {
+    getUserAuthData, isUserAdmin, isUserManager, userActions,
+} from '../../../../entities/User';
+import { Avatar } from '../../../../shared/ui/Avatar/Avatar';
+import { RoutePath } from '../../../../shared/config/routerConfig/routerConfig';
+import { classNames } from '../../../../shared/lib/classNames/classNames';
+
+interface AvatarDropdownProps {
+    className?: string;
+}
+
+export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const isAdmin = useSelector(isUserAdmin);
+    const isManager = useSelector(isUserManager);
+    const authData = useSelector(getUserAuthData);
+
+    const onLogOut = useCallback(() => {
+        dispatch(userActions.logOut());
+    }, [dispatch]);
+
+    const isAdminPanelAvailable = isAdmin || isManager;
+
+    if (!authData) {
+        return null;
+    }
+
+    return (
+
+        <Dropdown
+            direction="bottom left"
+            className={classNames('', {}, [className])}
+            items={[
+
+                ...(isAdminPanelAvailable ? [{
+                    content: t('Admin Panel'),
+                    href: RoutePath.admin_panel,
+                }] : []), // разворачиваем массив и в нем условие, если isAdminPanelAvailable,
+                // то у нас отрисовывается массив с админ панелью, если нет, то пустой
+                {
+                    content: t('Profile'),
+                    href: RoutePath.profile + authData.id,
+                },
+                {
+                    content: t('Sign out'),
+                    onClick: onLogOut,
+                },
+            ]}
+            trigger={<Avatar size={30} src={authData.avatar} />}
+        />
+    );
+});
